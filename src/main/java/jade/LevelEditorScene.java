83fa571f13b2a8e70d069602b066c698ee45f42e
Muhamad Nur Syami <nursami385@gmail.com>
2024-04-25 13:57:30 +0700

    feat : membuat component sistem

--- src/main/java/jade/LevelEditorScene.java
+++ src/main/java/jade/LevelEditorScene.java
@@ -1,5 +1,6 @@
 package jade;
-
+import components.FontRenderer;
+import components.SpriteRenderer;
 import org.joml.Vector2f;
 import org.lwjgl.BufferUtils;
 import renderer.Shader;
@@ -38,6 +39,9 @@ public class LevelEditorScene extends Scene {
     private  int voaID, vboID, eboID;
     private Shader defaultShader;
     private Texture testTexture;
+
+    GameObject testObj;
+    private boolean firstTime = false;
     public  LevelEditorScene(){
 
     }
@@ -45,6 +49,13 @@ public class LevelEditorScene extends Scene {
 
     @Override
     public void init(){
+
+        System.out.println("Creating 'test object'");
+        this.testObj = new GameObject("test object");
+        this.testObj.addComponent(new SpriteRenderer());
+        this.testObj.addComponent(new FontRenderer());
+        this.addGameObjectToScene(this.testObj);
+
         this.camera = new Camera(new Vector2f(-200, -300));
         defaultShader = new Shader("assets/shaders/default.glsl");
         defaultShader.compile();
@@ -125,6 +136,18 @@ public class LevelEditorScene extends Scene {
         glBindVertexArray(0);
 
         defaultShader.detach();
+        if (!firstTime) {
+            System.out.println("Creating gameObject!");
+            GameObject go = new GameObject("Game Test 2");
+            go.addComponent(new SpriteRenderer());
+            this.addGameObjectToScene(go);
+            firstTime = true;
+        }
+
+
+        for (GameObject go : this.gameObjects) {
+            go.update(dt);
+        }
     }
 
 
